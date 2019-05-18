54976b725b0dd79e484048dce987ef49b30f7453
rmit-s3511667-Ali-Alahmari <32062234+AliAla@users.noreply.github.com>
2019-05-19 01:18:40 +1000

    updating file

---
+++ RPi/RPi.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# Reference: https://realpython.com/python-sockets/
+# Documentation: https://docs.python.org/3/library/socket.html
+
+import socket_utils
+import socket
+import json
+import sqlite3
+import sys
+sys.path.append("..")
+
+with open("config.json", "r") as file:
+    data = json.load(file)
+
+HOST = data["masterpi_ip"]  # The server's hostname or IP address.
+PORT = 63001               # The port used by the server.
+ADDRESS = (HOST, PORT)
+
+
+def login(user):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        print("Connecting to {}...".format(ADDRESS))
+        s.connect(ADDRESS)
+        print("Connected.")
+
+        print("Logging in as {}".format(user["username"]))
+        socket_utils.sendJson(s, user)
+
+        print("Waiting for Master Pi...")
+        while(True):
+            object = socket_utils.recvJson(s)
+            if("logout" in object):
+                print("Master Pi logged out.")
+                print()
+                break
