822fa2598d1bdebbe2ef700652b3f274f223c560
rmit-s3511667-Ali-Alahmari <32062234+AliAla@users.noreply.github.com>
2019-05-21 13:30:35 +1000

    Updating app file and reciptionist file

--- RPi/App.py
+++ RPi/App.py
@@ -1,51 +1,49 @@
+#!/usr/bin/env python
 from receptionist import Receptionist
 import sys
 import RPi
-from userDB import UserDB
 from FacialRecognition_Drive import FaceRecognition_Drive
 
 
-def menu():
-    re = Receptionist()
-    drive = FaceRecognition_Drive()
-    print("""
-=====================================================
-   *          *                    *       *  *  *       
-   *          *  *               * *      *        *     
-   *          *    *           *   *        * 
-   *          *      *       *     *          *
-   *          *        *   *       *     *       *
-   * * * *    *          *         *      * *  *  *
-======================================================
-        """)
-    while True:
-        print("  ************MAIN MENU**************")
-        choice = input("""
-    1: Register
-    2: Log in using username and passward
-    3: Log in using face recognation
-    4: Quit/Log Out
+class App:
+    def menu():
+        re = Receptionist()
+        drive = FaceRecognition_Drive()
+        print("""
+    =====================================================
+    *          *                    *       *  *  *       
+    *          *  *               * *      *        *     
+    *          *    *           *   *        * 
+    *          *      *       *     *          *
+    *          *        *   *       *     *       *
+    * * * *    *          *         *      * *  *  *
+    ======================================================
+            """)
+        while True:
+            print("  ************MAIN MENU**************")
+            choice = input("""
+        1: Register
+        2: Log in using username and passward
+        3: Log in using face recognation
+        4: Quit/Log Out
 
-    Please enter your choice: """)
-        if choice.isdigit():
-            break
-        print("You must only select either 1,2,3 or 4.")
-        print("Please try again")
+        Please enter your choice: """)
 
-        if choice == '1':
-            re.register()
-        elif choice == '2':
-            name = re.login()
-            if name is not False:
-                RPi.login(name)
-        elif choice == '3':
-            data = drive.face_Recognation()
-            RPi.login(data)
-        elif choice == '4':
-            sys.exit()
-        else:
-            print("You must only select either 1,2,3 or 4.")
-            print("Please try again")
+            if choice == '1':
+                re.register()
+            elif choice == '2':
+                name = re.login()
+                if name is not False:
+                    RPi.login(name)
+            elif choice == '3':
+                data = drive.face_Recognation()
+                RPi.login(data)
+            elif choice == '4':
+                sys.exit()
+            else:
+                print("You must only select either 1,2,3 or 4.")
+                print("Please try again")
 
 
-menu()
+if __name__ == "__main__":
+    App().menu()
