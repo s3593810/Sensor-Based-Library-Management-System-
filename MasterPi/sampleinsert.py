67aaff387e58a2d5a7af0e790f2dfe9cdf6bc2a3
unknown <s3593810@student.rmit.edu.au>
2019-05-23 23:58:02 +1000

    Comments added for documentation

--- MasterPi/sampleinsert.py
+++ MasterPi/sampleinsert.py
@@ -3,11 +3,65 @@ from datetime import datetime
 import time
 
 class SamInsert:
+    
+    """
+    This class shows the cloud managment and database 
+    functionalities.
+
+    
+    Attributes
+    ----------
+
+    HOST
+        IP address of the Host device.
+    USER
+        UserName for the Cloud database.
+    PASSWORD
+        Password for the cloud database.
+    DATABASE
+        Name of the database already created in cloud.
+        
+    Methods
+    -------
+    __init__(connection=None)
+        Checks on the connection and selects cursor 
+        cleans up the database if there is already tables exist.
+
+    close()
+        Closes the connection.
+    __enter__()
+
+    __exit__()
+
+    insertUser(name, nick)
+        Creates a user for LMS User table in the cloud database
+        based on the given username and name of the user.
+
+    insertBook(name, author, time)
+        Creates a book based on provided title, author name of the 
+        book and the time in book table in the cloud database.
+
+    getPeople()
+        This method helps getting the username and name of the user 
+        from the LMS User table.
+    
+    getPerson(id)
+        This method helps getting the username and name from lms user table
+        based on provided users id.
+
+    getBook(id)
+        This method helps getting the book information from book table
+        based on provided book id.
+
+
+    """
+
     HOST = "35.189.19.149"
     USER = "root"
     PASSWORD = "Sami9305"
     DATABASE = "lms"
 
+    
     def __init__(self, connection = None):
         if(connection == None):
             connection = MySQLdb.connect(SamInsert.HOST, SamInsert.USER,
