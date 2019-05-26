1c02ab41268f1280181530f60679966d045336ac
unknown <s3593810@student.rmit.edu.au>
2019-05-26 23:13:56 +1000

    Documentation HTML Generated

---
+++ Documentatio/RPiCode/App.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# from receptionist import Receptionist
+# import sys
+# from RPi import ReceptionPi
+# from userDB import UserDB
+# from FacialRecognition_Drive import FaceRecognition_Drive
+
+
+class App:
+    """
+    This class shows the Library Management System
+    Menu options as well as ensures basic functionalities.
+
+    ...
+
+    Methods
+    -------
+    menu()
+        This method shows and runs the main functionalities 
+        of the Library Managment System consol application.
+
+    """
+    def menu():
+        # re = Receptionist()
+        # drive = FaceRecognition_Drive()
+        # receptionPi = ReceptionPi()
+        print("""
+    =====================================================
+    *          *                    *       *  *  *       
+    *          *  *               * *      *        *     
+    *          *    *           *   *        * 
+    *          *      *       *     *          *
+    *          *        *   *       *     *       *
+    * * * *    *          *         *      * *  *  *
+    ======================================================
+            """)
+        while True:
+            print("  ************MAIN MENU**************")
+            choice = input("""
+        1: Register
+        2: Log in using username and passward
+        3: Log in using face recognation
+        4: Quit/Log Out
+
+        Please enter your choice: """)
+
+            if choice == '1':
+                # re.register()
+            # elif choice == '2':
+                # name = re.login()
+                # if name is not False:
+                    # receptionPi.login(name)
+
+            # elif choice == '3':
+                # data = drive.face_Recognation()
+                # receptionPi.login(name)
+            # elif choice == '4':
+                # sys.exit()
+            # else:
+                print("You must only select either 1,2,3 or 4.")
+                print("Please try again")
+
+
+if __name__ == "__main__":
+    App().menu()
---
+++ Documentatio/rst/index.rst
@@ -0,0 +1,23 @@
+.. LMS documentation master file, created by
+   sphinx-quickstart on Tue May 21 15:37:01 2019.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to LMS's documentation!
+===============================
+This the documentation for the Final Project IoT, Semester 2, 2019.
+This projecrt is an smart library system to allow users interact digitally 
+to borrow, return or search for books.
+Special Funtionality Includes: 
+
+User can use QR scanner Mechanism to Borrow or Return a Book.
+User can use voice Recognition to Search for Book.
+User can use Facial Recognition Technologies to Login to the System.
+
+.. toctree::
+   :maxdepth: 4
+   :caption: Contents:
+   
+   MasterPy
+   ReceptionPy
+   Flask_Site_and_Api
