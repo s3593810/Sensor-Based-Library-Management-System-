a2a59f8d0059ba581595f81c4b4e7aecf232b296
rmit-s3675153-suneeth-gs <32081315+rmit-s3675153-suneeth-gs@users.noreply.github.com>
2019-05-26 23:48:18 +1000

    flask_final_2

---
+++ flask/iot_main.py
@@ -0,0 +1,26 @@
+# pip3 install flask flask_sqlalchemy flask_marshmallow marshmallow-sqlalchemy
+# python3 flask_main.py
+from flask import Flask, request, jsonify, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import os, requests, json
+from iot_api import api, db
+
+app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Update HOST and PASSWORD appropriately.
+HOST = "35.244.105.250"
+USER = "root"
+PASSWORD = "abc123"
+DATABASE = "LMS"
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://{}:{}@{}/{}".format(USER, PASSWORD, HOST, DATABASE)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+
+db.init_app(app)
+
+app.register_blueprint(api)
+
+if __name__ == "__main__":
+    app.run(host = "127.0.0.1",port ="5000")
---
+++ flask/r.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set FLASK_APP=iot_main.py
+export FLASK_APP=iot_main.py
+flask run --host 127.0.0.1
