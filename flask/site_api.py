1234332c2c3a6f8db43c61807e1e11ab517fce25
unknown <s3593810@student.rmit.edu.au>
2019-05-26 19:15:02 +1000

    added comments for documentation

--- flask/site_api.py
+++ flask/site_api.py
@@ -3,20 +3,53 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 import os, requests, json
 
+"""
+This site_api class inegrates the api with the site view
+and helps geeting, posting and delating functions through api calls wiythin in 
+the cloud thus acts as the client for the site.
+
+Methods
+-------
+
+index()
+    it srats up based on the index template.
+    It starts as landing site with basic functional
+    features in the site.
+
+do_admin_login()
+    Admin login functionalities through api get and post requests
+    as user types in user name and password thus it posts the request to 
+    the api for login and the api checks the user name and password with the 
+    existing database thus it gets api results.
+    
+dashboard()
+    This method opens app the dashbord site view based on rendering template
+    dashboard.html.
+report()
+    This method helps getting the response ass all the books in the database 
+    as rendered site as report.html.
+addmenu()
+    Allows user to type in book information through the site to add a book
+    through the api calls to the cloud database.
+delete()
+    Using restful api calls this method allows user to delate 
+    a book from cloud database based on users action on the site. 
+"""
+
 app = Flask(__name__)
 app.secret_key = 'super secret key'
 
 # Client webpage.
 @app.route("/")
 def index():
-    print("test1")
+    
     return render_template("index.html")
 
 @app.route('/login', methods=['GET','POST'])
 def do_admin_login():
-    print("tada")
+    
     if request.form['password'] == 'hghar' and request.form['username'] == 'jaqen':
-        print("true")
+        
         session['logged_in'] = True
         return redirect(url_for('dashboard'))
     else:
@@ -65,7 +98,7 @@ def bookadd():
 @app.route('/delete',  methods = ['POST'])
 def bookdelete():
     # Use REST API.
-    print("Am i working ???")
+    
     bookid = request.form['BookID']
 # sending post request and saving response as response object 
     e = requests.delete(url = "http://localhost:5000/api/book/"+bookid) 
